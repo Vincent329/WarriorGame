90ef16df2cc349ba0041e578094504d3023535c3
Vincent329 <vincee.ho@gmail.com>
2025-02-20 14:46:31 -0500

    forward declaration to add springarm and camera components to the hero character

--- Source/WarriorGame/Public/Characters/WarriorHeroCharacter.h
+++ Source/WarriorGame/Public/Characters/WarriorHeroCharacter.h
@@ -4,8 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "WarriorDebugHelper.h"
+//#include "GameFramework/SpringArmComponent.h"
+//#include "Components/CameraComponent.h"
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
+
+// forward declaration of the Spring Arm and Camera Components
+// this will help speed up compile times
+class USpringArmComponent;
+class UCameraComponent;
 
 /**
  * 
@@ -14,7 +21,21 @@ UCLASS()
 class WARRIORGAME_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
 	
 protected:
 	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+	USkeletalMeshComponent* SkeletalMesh;
+#pragma endregion
 };
