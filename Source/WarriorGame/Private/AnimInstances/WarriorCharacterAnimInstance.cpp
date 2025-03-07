3e71032a4bdd440400b7bb7ac23daf8c4cd25d30
Vincent329 <vincee.ho@gmail.com>
2025-03-07 17:45:27 -0500

    [MODIFY] - Change so that the code logic is under Character Anim Instance, not Hero

--- Source/WarriorGame/Private/AnimInstances/WarriorCharacterAnimInstance.cpp
+++ Source/WarriorGame/Private/AnimInstances/WarriorCharacterAnimInstance.cpp
@@ -2,4 +2,29 @@
 
 
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UWarriorCharacterAnimInstance::NativeInitializeAnimation()
+{
+	OwningCharacter = Cast<AWarriorBaseCharacter>(TryGetPawnOwner());
+
+	// nativeinitialize animation is called in the editor
+	// can't use check macro here
+	if (OwningCharacter)
+	{
+		OwningMovementComponent = OwningCharacter->GetCharacterMovement();
+	}
+}
+
+void UWarriorCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	if (!OwningCharacter || !OwningMovementComponent)
+	{
+		return;
+	}
+
+	f_GroundSpeed = OwningCharacter->GetVelocity().Size2D();
+
+	b_HasAcceleration = OwningMovementComponent->GetCurrentAcceleration().SizeSquared2D() > 0.0f; // essentially returning the magnitude of the acceleration (2D vector magnitude if we're just getting the lateral accelaration).
+}
--- Source/WarriorGame/Public/AnimInstances/WarriorCharacterAnimInstance.h
+++ Source/WarriorGame/Public/AnimInstances/WarriorCharacterAnimInstance.h
@@ -6,6 +6,9 @@
 #include "AnimInstances/WarriorAnimInstance.h"
 #include "WarriorCharacterAnimInstance.generated.h"
 
+class AWarriorBaseCharacter;
+class UCharacterMovementComponent;
+
 /**
  * 
  */
@@ -13,5 +16,23 @@ UCLASS()
 class WARRIORGAME_API UWarriorCharacterAnimInstance : public UWarriorAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+
+	// no need to call super in any of these classes
+	virtual void NativeInitializeAnimation() override; // override Initialize Animation function from the base AnimInstance class
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;  // override thread safe update animation function.  Use Thread Safe to have this run on a separate thread, not game thread
+																				// acts similarly to our tick function, but works on worker thread, not game thread
+																				// optimized so that our animation blueprints can avoid bottlenecks
+protected:
+	UPROPERTY()
+	AWarriorBaseCharacter* OwningCharacter;
+
+	UPROPERTY()
+	UCharacterMovementComponent* OwningMovementComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float f_GroundSpeed;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool b_HasAcceleration;
 };
