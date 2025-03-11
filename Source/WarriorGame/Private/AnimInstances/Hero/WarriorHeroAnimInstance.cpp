4292fd0975e239ef2d6e79bbde6ea7749547207d
Vincent329 <vincee.ho@gmail.com>
2025-03-11 00:50:21 -0400

    [MODIFY] - Warrior Hero Anim Instance logic filled out

--- Source/WarriorGame/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/WarriorGame/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -2,3 +2,32 @@
 
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// we are getting our Owning Character from the parent class, it's just a Base Class
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter); // This is coming from the header, this will be our hero character reference
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (b_HasAcceleration)
+	{
+		f_IdleElapsedTime = 0.0f;
+		b_ShouldEnterRelaxState = false;
+		
+	}
+	else
+	{
+		f_IdleElapsedTime += DeltaSeconds;
+		b_ShouldEnterRelaxState = (f_IdleElapsedTime >= f_EnterRelaxThreshold); // change the relax state to true if elapsed time greater than or equal to the relax threshold
+	}
+}
--- Source/WarriorGame/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/WarriorGame/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,8 +6,7 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
-class AWarriorBaseCharacter;
-class UCharacterMovementComponent;
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -16,5 +15,20 @@ class WARRIORGAME_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimIns
 {
 	GENERATED_BODY()
 
-				
+public:
+	virtual void NativeInitializeAnimation() override; // override Initialize Animation function from the base AnimInstance class
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;  // override thread safe update animation function.  Use Thread Safe to have this run on a separate thread, not game thread
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool b_ShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData") // we want to edit the threshold timer in editor
+	float f_EnterRelaxThreshold = 5.f;				// should we idle for more than 5 seconds, we are in relax stance
+
+	float f_IdleElapsedTime = 0.f;						// keeping track of the Idle Elapsed Time
+
 };
