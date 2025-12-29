4c0f6878ada29bc73e1ae1d38aff567795f992f1
Vincent329 <vincee.ho@gmail.com>
2025-12-29 18:57:38 -0500

    5.7

--- Source/WarriorGame.Target.cs
+++ Source/WarriorGame.Target.cs
@@ -8,8 +8,8 @@ public class WarriorGameTarget : TargetRules
 	public WarriorGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("WarriorGame");
 	}
 }
