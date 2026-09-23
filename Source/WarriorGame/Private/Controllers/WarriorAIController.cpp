cde0f01461351ec2c9c70b41aeb3f93ffd7e4f3a
Vincent329 <vincee.ho@gmail.com>
2026-09-23 19:21:41 -0400

    Add a Perception and Sight Config component
    - Create Perception Component
    - Create Sight Config for Sense
    - defined both variables in the constructor
    - Created a binding for when the targets are detected

--- Source/WarriorGame/Private/Controllers/WarriorAIController.cpp
+++ Source/WarriorGame/Private/Controllers/WarriorAIController.cpp
@@ -3,6 +3,9 @@
 
 #include "Controllers/WarriorAIController.h"
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
 #include "WarriorDebugHelper.h"
 
 
@@ -13,4 +16,21 @@ AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectIniti
 	{
 		Debug::Print(TEXT("CrowdFollowingComponent Valid"), FColor::Green);
 	}
+
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("EnemySenseConfig_Sight");
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true; // detect anyone that isn't a team player
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f; // detection angle
+
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnTargetPerceptionUpdated);
+}
+
+void AWarriorAIController::OnTargetPerceptionUpdated(AActor * Actor, FAIStimulus AIStimulus)
+{
 }
